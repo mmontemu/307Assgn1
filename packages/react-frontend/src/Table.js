a4ef70f30ccd32b49500b7fa78e5cfc153f6e08e
Michael <mvmonte12@gmail.com>
2024-01-16 23:05:58 -0800

    Got up to step 10 on the lab

--- packages/react-frontend/src/Table.js
+++ packages/react-frontend/src/Table.js
@@ -7,6 +7,7 @@ function TableHeader() {
         <tr>
           <th>Name</th>
           <th>Job</th>
+          <th>Delete</th>
         </tr>
       </thead>
     );
@@ -16,9 +17,14 @@ function TableHeader() {
     const rows = props.characterData.map((row, index) => {
       return (
         <tr key={index}>
-          <td>{row.name}</td>
-          <td>{row.job}</td>
-        </tr>
+        <td>{row.name}</td>
+        <td>{row.job}</td>
+        <td>
+          <button onClick={() => props.removeCharacter(index)}>
+            Delete
+          </button>
+        </td>
+      </tr>
       );
      }
     );
@@ -30,11 +36,14 @@ function TableHeader() {
   }
   
   function Table(props) {
-      return (
-        <table>
-          <TableHeader />
-          <TableBody characterData={props.characterData} />
-        </table>
-      );
+    return (
+      <table>
+        <TableHeader />
+        <TableBody
+          characterData={props.characterData}
+          removeCharacter={props.removeCharacter}
+        />
+      </table>
+    );
   }
   export default Table;
